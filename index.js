bf7d75357af0675423f4fa98ddf53ee0811c7fdf
rubel5428 <rubelbabu5428@gmail.com>
2023-06-18 08:41:32 +0600

    issue fixd

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const verifyJWT = (req, res, next) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const usersCollection = client.db('assignment-12').collection('users');
         const classCollection = client.db('assignment-12').collection('class');
